dcff568edc1aeed7de9af7c86c66fc783cc3f1bb
ashefd <lokchan.tshanon@gmail.com>
2022-02-22 15:48:02 +0100

    SCD4X

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,19 +2,52 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFun_VEML7700_Arduino_Library.h>
+#include "SparkFun_SCD4x_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD4x
 
 
-#define Console SerialUSB
+SCD4x mySensor;
 
-void setup() {
-  // put your setup code here, to run once:
-    Console.begin(115200);
-    Console.println(F("Starting"));
+void setup()
+{
+  SerialUSB.begin(9600);
+  SerialUSB.println(F("SCD4x Example"));
+  Wire.begin();
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+  //mySensor.enableDebugging(); // Uncomment this line to get helpful debug messages on SerialUSB
+
+  //.begin will start periodic measurements for us (see the later examples for details on how to override this)
+
+  if (mySensor.begin() == false)
+  {
+    SerialUSB.println(F("Sensor not detected. Please check wiring. Freezing..."));
+    while (1)
+      ;
+  }else{
+    SerialUSB.println(F("Sensor detected"));
+  }
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  
+  if (mySensor.readMeasurement()) // readMeasurement will return true when fresh data is available
+  {
+    SerialUSB.println();
+
+    SerialUSB.print(F("CO2(ppm):"));
+    SerialUSB.print(mySensor.getCO2());
+
+    SerialUSB.print(F("\tTemperature(C):"));
+    SerialUSB.print(mySensor.getTemperature(), 1);
+
+    SerialUSB.print(F("\tHumidity(%RH):"));
+    SerialUSB.print(mySensor.getHumidity(), 1);
+
+    SerialUSB.println();
+  }
+  else
+    SerialUSB.print(F("."));
+
+  delay(500);
 }
